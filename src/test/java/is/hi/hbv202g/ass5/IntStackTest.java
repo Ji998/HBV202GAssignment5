b300815805def129d0e76c9dacc8bb63831529d5
Adrian Roszkowski <adr11@hi.is>
2023-02-23 19:14:45 +0000

    Improved test cases

--- src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
+++ src/test/java/is/hi/hbv202g/ass5/IntStackTest.java
@@ -11,17 +11,29 @@ public class IntStackTest {
     IntStack stack;
 
     @Before
-    public void testNewStackIsNotFull() {
+    public void initalizeTestVariables() {
         stack= new IntStack();
+    }
 
+    @Test
+    public void testNewStackIsNotFull() {
         assertFalse(stack.isFull());
     }
 
+    @Test
     public void testFullStackIsFull(){
-        for (int i = 0; i < stack.getCapacity(); i++) {
+        for (int i = 0; i < stack.getCapacity() - 1; i++) {
             stack.push(1);
         }
         assertTrue(stack.isFull());
+    }
+
+    @Test
+    public void testAlmostFull(){
+        for (int i = 0; i < stack.getCapacity() - 2; i++) {
+            stack.push(1);
+        }
+        assertFalse(stack.isFull());
     }
 
 }
